658ddc873be1ae34096297598b315aaee697886c
fuku5 <tokyo@keio.jp>
2021-11-30 12:06:47 +0900

    implement goal change mode

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name='multiple_lunar_lander',
-    version='0.0.4',
+    version='0.0.5',
     description='openai gym lunar lander with changing goals',
     long_description='README.md',
     author='YF',
     author_email='',
     url='',
     install_requires=['numpy', 'gym'],
-    license=license,
+    #license=license,
     packages=['multi_lunar']
 )
