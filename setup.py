bc28412cd5454821d830703c7922f2c27e3f4acb
fuku5 <tokyo@keio.jp>
2022-04-09 00:13:19 +0900

    add requirement

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name='multiple_lunar_lander',
-    version='0.0.4',
+    version='0.0.5',
     description='openai gym lunar lander with changing goals',
     long_description='README.md',
     author='YF',
     author_email='',
     url='',
-    install_requires=['numpy', 'gym'],
-    license=license,
+    install_requires=['numpy', 'gym', 'pyglet==1.3.2'],
+    license='licence',
     packages=['multi_lunar']
 )
